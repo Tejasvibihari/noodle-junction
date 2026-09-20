c5df26ddc75581351b7d515b4ceccb4bbcff3d7b
Tejasvi Kumar <tejasvibihari2000@gmail.com>
2026-09-20 18:57:09 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
